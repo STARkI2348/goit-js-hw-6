11935fdc22fd67133d3ca954cd74e3551b923f6a
STARkI2348 <107957195+STARkI2348@users.noreply.github.com>
2025-02-10 18:24:31 +0200

    Update test-1.js

--- js/test-1.js
+++ js/test-1.js
@@ -1,49 +1,22 @@
-class Car {
-  #brand;
-  #model;
-  #price;
-
-  constructor(params) {
-    this.#brand = params.brand;
-    this.#model = params.model;
-    this.#price = params.price;
+class User {
+  constructor(email) {
+    this.email = email;
   }
 
-  get brand() {
-    return this.#brand;
+  get email() {
+    return this.email;
   }
 
-  set brand(newBrand) {
-    this.#brand = newBrand;
-  }
-
-  get model() {
-    return this.#model;
-  }
-
-  set model(newModel) {
-    this.#model = newModel;
-  }
-
-  get price() {
-    return this.#price;
-  }
-
-  set price(newPrice) {
-    this.#price = newPrice;
+  set email(newEmail) {
+    this.email = newEmail;
   }
 }
 
-const car = new Car({
-  brand: "BMW",
-  model: "X5",
-  price: "40000"
-});
+class Admin extends User{
+  static role = {
+    BASIC: "basic", 
+    SUPERUSER: "superuser"
+  }
+}
 
-console.log(car.price);
-car.price = "35000";
-console.log(car.price);
-
-console.log(car.brand);
-car.brand = "Mers";
-console.log(car.brand);
+console.log(Admin.role.BASIC);
